c9d014e711613682e8f53891faa02d3addd119d1
AlexSickert <alex@alexandersickert.com>
2017-11-21 12:04:03 +0200

    initial version

--- get_includes.py
+++ get_includes.py
@@ -6,12 +6,14 @@ this file provides the static files and includes etc.
 
 
 """
+from pathlib import Path
 
 print("loading get_includes.py")
 
+
 def get_include_file_content(p):
-    
-     
+    f = ""
+
     if p == "/":
         f = './html/index.html'
     if "/include/Controller.js" in p:
@@ -20,12 +22,21 @@ def get_include_file_content(p):
         f = './html/app.html'
     if p == "/include/DataAccess.js":
         f = './js/DataAccess.js'
-        
+
     if p == "/include/UxUi.js":
         f = './js/UxUi.js'
-   
-    file = open(f, 'rb') 
-    s = file.read()   
-    file.close()
-    return s
 
+    my_file = Path(f)
+
+    if my_file.is_file():
+
+        file = open(f, 'rb')
+        s = file.read()
+        file.close()
+    else:
+        print('could not find the follwing file and hence return error: ' + str(p))
+        x = "error"
+        s = bytearray()
+        s.extend(map(ord, x))
+
+    return s
--- server.py
+++ server.py
@@ -25,53 +25,84 @@ base http server https://wiki.python.org/moin/BaseHttpServer
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import get_includes as gi
 import process_post as po
+import sys
+import datetime
+import time
 
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
-
         self.send_header('Content-type', 'text/html')
-
         self.send_header('Set-Cookie', 'B=412341234')
-
         self.end_headers()
 
     def do_GET(self):
+        start_execution = datetime.datetime.now()
         print("------------------ start GET ----------------------")
 
-        self._set_headers()
-        p = self.path
+        try:
 
-        print(self.path)
-        print(self.headers)
+            p = self.path
+            print(self.path)
+            print(self.headers)
 
-        s = gi.get_include_file_content(p)
+            s = gi.get_include_file_content(p)
 
-        # now write response
-        self.wfile.write(s)
+            # now write response
+            self._set_headers()
+            self.wfile.write(s)
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            self._set_headers()
+            s = "error"
+            b = bytearray()
+            b.extend(map(ord, s))
+            self.wfile.write(b)
 
+
+        end_execution = datetime.datetime.now()
+        execution_time_diff = end_execution - start_execution
+        millis = execution_time_diff.total_seconds() * 1000
+        print("GET execution took time in milliseconds: " + str(millis))
         print("-------------------end GET ---------------------")
 
     def do_HEAD(self):
         self._set_headers()
 
     def do_POST(self):
+        start_execution = datetime.datetime.now()
         print("------------- start POST ------------------")
+
         # Doesn't do anything with posted data
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
 
+        try:
+            print(post_data)
+            # process post data and make response
+            ret = po.process_post(post_data)
 
-        print(post_data)
-        # process post data and make response
-        ret = po.process_post(post_data)
+            # write response
+            self._set_headers()
+            self.wfile.write(ret.encode())
 
-        # write response
-        self._set_headers()
-        self.wfile.write(ret.encode())
+        except:
 
+            print("Unexpected error:", sys.exc_info()[0])
+            self._set_headers()
+            s = "error"
+            b = bytearray()
+            b.extend(map(ord, s))
+            self.wfile.write(b)
+
+
+        end_execution = datetime.datetime.now()
+        execution_time_diff = end_execution - start_execution
+        millis = execution_time_diff.total_seconds() * 1000
+        print("POST execution took time in milliseconds: " + str(millis))
         print("-------------- end POST --------------------------")
+
 
 
 def run(server_class=HTTPServer, handler_class=S, port=80):
@@ -82,9 +113,12 @@ def run(server_class=HTTPServer, handler_class=S, port=80):
 
 
 if __name__ == "__main__":
+
     from sys import argv
+
+    print("-------------- START SERVER --------------------------")
 
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
-        run()
+        run(port = 55555)
