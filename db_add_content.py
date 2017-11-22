651b57885048a8d08fc5eb1304ccc21ff1dcb3f7
AlexSickert <alex@alexandersickert.com>
2017-11-23 00:17:07 +0200

    changed db connection

--- db_add_content.py
+++ db_add_content.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import psycopg2
+import sys, traceback
 
 print("loading db_add_content.py")
 
@@ -8,24 +9,39 @@ print("loading db_add_content.py")
 def add_one_word(x):
     
     hostname = 'localhost'
-    username = 'testuser'
+    username = 'postgres'
     password = 'password'
     database = 'brainextension'
-    
-    conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
-    
-    cur = conn.cursor()
-    xxx = x
-    cur.execute( "insert into user_1_languages (language) values (%s);", (xxx,) )
-    conn.commit()
-    conn.close()
-    
-    conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
-        
-    cur = conn.cursor()
 
-    cur.execute( "SELECT id, language FROM user_1_languages;" )
+    print("111  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    try:
     
+        conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+
+        cur = conn.cursor()
+        xxx = x
+        yyy = x
+        cur.execute( "insert into users (email, password) values (%s, %s);", (xxx,yyy) )
+        # cur.execute("insert into user_1_languages (language) values (%s);", (xxx,))
+        conn.commit()
+        conn.close()
+
+        print("222  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+        conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+
+        cur = conn.cursor()
+
+        # cur.execute( "SELECT id, language FROM user_1_languages;" )
+        cur.execute("SELECT id, email FROM users;")
+
+        print("333  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    except:
+
+        print ("Exception in user code:")
+        print ('-'*60)
+        traceback.print_exc(file=sys.stdout)
+        print ('-'*60)
   
 #
 #    for a, b in cur.fetchall() :
