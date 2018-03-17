12b097c00936b9b1dd239a165d512b1039d7e6cf
AlexSickert <alex@alexandersickert.com>
2018-03-17 14:08:32 +0200

    various changes

--- log.py
+++ log.py
@@ -6,13 +6,25 @@ ToDo make this a separate thread and the methods just put items in a queue
 """
 
 
+import datetime
+import time
+
+
+
+
 def log_error(x):
-    print("ERROR:: " + str(x))
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    print("ERROR:: " + st + " :: " + str(x))
 
 
 def log_info(x):
-    print("INFO:: " + str(x))
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    print("INFO:: " + st + " :: " + str(x))
 
 
 def log_warning(x):
-    print("WARNING:: " + str(x))
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    print("WARNING:: " + st + " :: " + str(x))
