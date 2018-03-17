71eeea7d083ddba6aac1eb831375ad9a0acf98d4
AlexSickert <alex@alexandersickert.com>
2018-03-17 14:06:37 +0200

    various changes

--- db_add_content.py
+++ db_add_content.py
@@ -2,60 +2,195 @@
 
 import psycopg2
 import sys, traceback
+import logfiles
+import config as cfg
 import log
 
 print("loading db_add_content.py")
 
 
-def add_one_word(x):
-    
-    hostname = 'localhost'
-    username = 'postgres'
-    password = 'password'
-    database = 'brainextension'
+def add_spreadsheet_list(user_id, language, language_translation, list_obj):
 
-    log.log_info("111  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    try:
-    
-        conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+    """
+    uses the json object that was received by google spreadsheet and adds the words to the database
+    :param user:
+    :param language:
+    :param list_obj:
+    :return:
+    """
 
+    lang_code = get_language_code(language)
+
+    log.log_info("add_spreadsheet_list adding words... ")
+
+    for row in list_obj:
+        voc = row[0]
+        trans = row[1]
+
+        if len(voc) > 0:
+            if len(trans) > 0:
+
+                add_one_word_txt(user_id, language, voc, language_translation, trans)
+
+                add_one_word_txt(user_id, language_translation, trans, language, voc)
+
+    return ""
+
+
+def add_one_word_txt(user_id, language_word, word, language_translation, translation):
+
+    language_word = get_language_code(language_word)
+    language_translation = get_language_code(language_translation)
+
+    add_one_word(user_id, language_word, word, language_translation, translation)
+
+def add_one_word(user_id, language_word, word, language_translation, translation):
+
+    """
+    Add one single word and its translation
+    :param user_id:
+    :param language:
+    :param word:
+    :param translation:
+    :return:
+    """
+
+    word = word.strip()
+
+    if word_exists(user_id, language_word, word, language_translation):
+        # print("word existst")
+        update_word(user_id, language_word, word, language_translation, translation)
+        return
+    else:
+        # print("new word")
+        conn = get_connection()
         cur = conn.cursor()
-        xxx = x
-        yyy = x
-        cur.execute( "insert into users (email, password) values (%s, %s);", (xxx,yyy) )
-        # cur.execute("insert into user_1_languages (language) values (%s);", (xxx,))
+        sql = "insert into vocabulary (user_id, language_word, word, language_translation, translation) values (%s, %s, %s , %s, %s);"
+        cur.execute(sql, (user_id, language_word, word, language_translation, translation))
         conn.commit()
-        conn.close()
+        # print("done")
+    return
 
-        log.log_info("222  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
-        conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+def get_connection():
+    """
+    gets the database connection.
+    :return:
+    """
 
-        cur = conn.cursor()
+    hostname = cfg.parameters["hostname"]
+    username = cfg.parameters["username"]
+    password = cfg.parameters["password"]
+    database = cfg.parameters["database"]
 
-        # cur.execute( "SELECT id, language FROM user_1_languages;" )
-        cur.execute("SELECT id, email FROM users;")
+    conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
 
-        log.log_info("333  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    except:
+    return conn
 
-        log.log_info ("Exception in user code:")
-        log.log_info ('-'*60)
-        traceback.print_exc(file=sys.stdout)
-        log.log_info ('-'*60)
-  
-#
-#    for a, b in cur.fetchall() :
-#        print(a)
-#        print(b)
-    for row in cur:
-        log.log_info(row[0])
-        log.log_info(row[1])
-        
-    conn.close()
-    
-    
-    
-    
-    
-    
+
+def update_word(user_id, language_word, word, language_translation, translation):
+
+    word = word.strip()
+
+    conn = get_connection()
+    cur = conn.cursor()
+    sql = "update vocabulary set translation = %s where user_id = %s and language_word = %s and word = %s and language_translation = %s;"
+    cur.execute(sql, (translation, user_id, language_word, word, language_translation))
+    conn.commit()
+
+    return True
+
+
+def word_exists(user_id, language_word, word, language_translation):
+
+    """
+    checks if a word exists
+    :param user:
+    :param language:
+    :param word:
+    :return: true or false
+    """
+
+    word = word.strip()
+
+    conn = get_connection()
+    cur = conn.cursor()
+    sql = "SELECT count(*)  FROM vocabulary where user_id = %s and word = %s and language_word = %s and language_translation = %s"
+    cur.execute(sql, (user_id, word, language_word, language_translation))
+
+    l = cur.fetchall()[0][0]
+
+    if l > 0:
+        return True
+    else:
+        return False
+
+
+def get_language_code(str):
+
+    """
+    convert the language strings into language codes to make database more compact
+    ToDo: make this a database table that gets loaded on startup
+    :param str:
+    :return:
+    """
+
+    s = str.strip().lower()
+
+    if s == "english":
+        return 1
+    if s == "en":
+        return 1
+    if s == "engl":
+        return 1
+
+    if s == "spanish":
+        return 2
+    if s == "es":
+        return 2
+    if s == "span":
+        return 2
+
+    if s == "german":
+        return 3
+    if s == "de":
+        return 3
+    if s == "deutsch":
+        return 3
+
+    if s == "russian":
+        return 4
+    if s == "ru":
+        return 4
+    if s == "rus":
+        return 4
+
+    if s == "italian":
+        return 5
+    if s == "it":
+        return 5
+    if s == "ital":
+        return 5
+    if s == "italiano":
+        return 5
+
+    if s == "portuguese":
+        return 6
+    if s == "pt":
+        return 6
+    if s == "port":
+        return 6
+    if s == "portugese":
+        return 6
+
+    if s == "french":
+        return 7
+    if s == "fr":
+        return 7
+    if s == "franz":
+        return 7
+
+    return 99
+
+
+
