817439f21cfab4f3200e9c8bc79e11261977cf4e
AlexSickert <alex@alexandersickert.com>
2018-03-17 14:06:55 +0200

    various changes

--- process_post.py
+++ process_post.py
@@ -4,11 +4,14 @@
 from urllib import parse
 import json
 import process_json as pj
+
 import log
 
 log.log_info("loading process_post.py")
 
 def process_post(header_values, form_values):
+
+    print("in process_post")
     
     """
     If we have a json object then we convert the string into a json object
@@ -21,13 +24,20 @@ def process_post(header_values, form_values):
     if "objJSON" in form_values:
         s = form_values["objJSON"]
 #        print("json object") 
-        x = s.decode('utf-8')
+#         x = s.decode('utf-8')
+        x = s
         j = parse.unquote(x)
         # j = j[8:]
-        data = json.loads(j)  
+        data = json.loads(j)
+
+        pj.distribute_actions(data)
+
+
+        # if asdfasd
+
         
         print(j)
-        res = pj.distribute_actions(data)
+
         
     else:
         """
