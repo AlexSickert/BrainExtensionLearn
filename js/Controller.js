d5605dd604bf609e262666e0c66cc3dc09baa4c3
AlexSickert <alex@alexandersickert.com>
2018-03-17 14:05:17 +0200

    various changes

--- js/Controller.js
+++ js/Controller.js
@@ -1,92 +1,129 @@
 /*
- 
- 
- 
+
+
+
  */
 
 
 
 function Controller() {
 
-    var objDataAccess = new DataAccess();
-    var objUxUi;
+  var objDataAccess = new DataAccess();
+  var objUxUi;
 
-    /*
-     * this function runs when the html page loads initially
-     */
-    this.init = function (uxui) {
+  /*
+   * this function runs when the html page loads initially
+   */
+  this.init = function(uxui) {
 
-        // set navigation and main body area
-        objUxUi = uxui;
-        objUxUi.setNavi("");
-        objUxUi.setLearnForm();
+    // set navigation and main body area
+    objUxUi = uxui;
+    objUxUi.setNavi("");
+    objUxUi.setLearnForm();
 
-        return;
-    };
+    return;
+  };
 
-    //===================================================
-    // function to handle Ui Interaction
-
-    this.setAddVocForm = function () {
-        objUxUi.setNavi("add");
-        objUxUi.setAddVocForm();
-        return;
-    };
-    
-    this.setLearnForm = function () {
-        objUxUi.setNavi("learn");
-        objUxUi.setLearnForm();
-        return;
-    };  
-    
-    this.setSettingsForm = function () {
-        objUxUi.setNavi("settings");
-        objUxUi.setSettingsForm();
-        return;
-    };   
-    
-    this.setResultsForm = function () {
-        objUxUi.setNavi("results");
-        objUxUi.setResultsForm();
-        return;
-    };       
-
-    /*
-     * loads a given url and its language from web and ads it to database
-     */
-    this.saveAddVoc = function () {
-        var l = objUxUi.getValue("adVocLanguage");
-        var u = objUxUi.getValue("adVocUrl");
-        var w = objUxUi.getValue("adVocWord");
-        var t = objUxUi.getValue("adVocText");
-        objUxUi.setAddVocFormMessage("processing...");
-
-	console.log("adVocLanguage = " + l);
-	console.log("adVocUrl = " + u);
-	console.log("adVocWord = " + w);
-	console.log("adVocText = " + t);
-
-        var r = new JsonAddVocRequest(l, u, w, t, "adVocFromUrl");
-        objDataAccess.ajaxPost(this, r);
-    };
+  //===================================================
 
 
+  this.testJson = function() {
 
-    //===================================================
-    // function to handle call back from Ajax
+    var r = new JsonAddVocRequest("ldghfg", "udfghdfg", "wdfghdfgh", "tdfhf", "testJson");
 
-    this.callBack = function (responseObject) {
+    objDataAccess.ajaxPostJson(this, "json", r);
 
-        console.log(responseObject["result"]);
-        console.log(responseObject["action"]);
-        
-        if(responseObject["action"] === "adVocFromUrl"){            
-            this.loadFromUrlResult(responseObject);            
-        }
-    };
-    
-    
-    this.loadFromUrlResult = function (responseObject) {          
-        objUxUi.setAddVocFormMessage(responseObject["result"]);        
-    };
+
+  }
+
+  //===================================================
+  // function to handle Ui Interaction
+
+  this.setAddVocForm = function() {
+    objUxUi.setNavi("add");
+    objUxUi.setAddVocForm();
+
+    // add value form cookies
+    objUxUi.setValue("adVocLanguage", this.getCookie("adVocLanguage"));
+    objUxUi.setValue("adVocTranslationLanguage", this.getCookie("adVocTranslationLanguage"));
+
+    return;
+  };
+
+  this.setLearnForm = function() {
+    objUxUi.setNavi("learn");
+    objUxUi.setLearnForm();
+    return;
+  };
+
+  this.setSettingsForm = function() {
+    objUxUi.setNavi("settings");
+    objUxUi.setSettingsForm();
+    return;
+  };
+
+  this.setResultsForm = function() {
+    objUxUi.setNavi("results");
+    objUxUi.setResultsForm();
+    return;
+  };
+
+  /*
+   * loads a given url and its language from web and ads it to database
+   */
+  this.saveAddVoc = function() {
+    var l = objUxUi.getValue("adVocLanguage");
+    var u = objUxUi.getValue("adVocUrl");
+    var w = objUxUi.getValue("adVocWord");
+    var t = objUxUi.getValue("adVocText");
+    var tl = objUxUi.getValue("adVocTranslationLanguage");
+    var tw = objUxUi.getValue("adVocTranslationWord");
+
+    // save in cookie
+    this.setCookie("adVocLanguage", l);
+    this.setCookie("adVocTranslationLanguage", tl);
+
+
+    objUxUi.setAddVocFormMessage("processing...");
+
+    console.log("adVocLanguage = " + l);
+    console.log("adVocUrl = " + u);
+    console.log("adVocWord = " + w);
+    console.log("adVocText = " + t);
+
+    var r = new JsonAddVocRequest(l, u, w, t, "adVocFromUrl", tl, tw);
+    objDataAccess.ajaxPost(this, r);
+  };
+
+
+  // handle cookies
+
+  this.setCookie = function(key, value) {
+    console.log("setCookie key = " + key);
+    console.log("setCookie value = " + value);
+    objDataAccess.setCookieValue(key, value);
+  };
+
+  this.getCookie = function(key) {
+    return objDataAccess.getCookieValue(key);
+  };
+
+
+  //===================================================
+  // function to handle call back from Ajax
+
+  this.callBack = function(responseObject) {
+
+    console.log(responseObject["result"]);
+    console.log(responseObject["action"]);
+
+    if (responseObject["action"] === "adVocFromUrl") {
+      this.loadFromUrlResult(responseObject);
+    }
+  };
+
+
+  this.loadFromUrlResult = function(responseObject) {
+    objUxUi.setAddVocFormMessage(responseObject["result"]);
+  };
 }
