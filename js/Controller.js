c5038c29e1f9952280947cad939a5174952606d5
AlexSickert <alex@alexandersickert.com>
2018-03-19 23:31:03 +0200

    handle yes/no response from user

--- js/Controller.js
+++ js/Controller.js
@@ -16,11 +16,13 @@ function Controller() {
    */
   this.init = function(uxui) {
 
+
     // set navigation and main body area
     objUxUi = uxui;
-    objUxUi.setNavi("");
-    objUxUi.setLearnForm();
 
+    //objUxUi.setNavi("");
+    //objUxUi.setLearnForm();
+    objUxUi.initializeTrainingGui();
     return;
   };
 
@@ -40,13 +42,15 @@ function Controller() {
   // function to handle Ui Interaction
 
   this.setAddVocForm = function() {
+    // hide the training html decoded
+    objUxUi.hideTrainingUi();
+    objUxUi.showDiv("mainNavi");
+    objUxUi.showDiv("mainBody");
     objUxUi.setNavi("add");
     objUxUi.setAddVocForm();
-
     // add value form cookies
     objUxUi.setValue("adVocLanguage", this.getCookie("adVocLanguage"));
     objUxUi.setValue("adVocTranslationLanguage", this.getCookie("adVocTranslationLanguage"));
-
     return;
   };
 
@@ -108,8 +112,31 @@ function Controller() {
     return objDataAccess.getCookieValue(key);
   };
 
+  //============================================================================
+  // Function to handle the interaction from the training Gui
 
-  //===================================================
+  this.answerYes = function(answer) {
+    var r = new JsonLoadWordRequest("YES",globalWordId);
+    objDataAccess.ajaxPost(this, r);
+  }
+
+  this.answerNo = function(answer) {
+    var r = new JsonLoadWordRequest("NO",globalWordId);
+    objDataAccess.ajaxPost(this, r);
+  }
+
+  this.answerQuestionmark = function() {
+    objUxUi.showHiddenWord();    
+  }
+
+  // initialize the loading. Result is handled by callback
+  this.loadWord = function() {
+    var r = new JsonLoadWordRequest("","");
+    objDataAccess.ajaxPost(this, r);
+  }
+
+
+  //============================================================================
   // function to handle call back from Ajax
 
   this.callBack = function(responseObject) {
@@ -119,10 +146,19 @@ function Controller() {
 
     if (responseObject["action"] === "adVocFromUrl") {
       this.loadFromUrlResult(responseObject);
+    } else if (responseObject["action"] === "loadWord") {
+      globalWordId = responseObject["wordId"];
+      var l1 = responseObject["language1"];
+      var l2 = responseObject["language2"];
+      var w1 = responseObject["word1"];
+      var w2 = responseObject["word2"];
+
+      objUxUi.setLearnFormValues(l1, w1, l2, w2);
     }
   };
 
 
+  // TiDo: Where is this used ???
   this.loadFromUrlResult = function(responseObject) {
     objUxUi.setAddVocFormMessage(responseObject["result"]);
   };
