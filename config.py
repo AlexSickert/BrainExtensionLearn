68e75e335b4efd074f150ca1ca815acb01952e25
AlexSickert <alex@alexandersickert.com>
2018-07-29 22:52:14 +0300

    various

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 
 import json
 
+
+# ToDo: If this does not load then the entore applicaiton should not load. Put it in a try catch and then shut down
+# the app if not loading
+
 parameters = json.load(open('./config.json'))
 
