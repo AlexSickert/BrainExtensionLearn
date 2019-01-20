09f74cd424d7e1b56e510a946d4a894329e058a4
AlexSickert <alex@alexandersickert.com>
2019-01-20 22:07:46 +0200

    initial version

--- process_get.py
+++ process_get.py
@@ -1,10 +1,32 @@
 
-
+import db_security as dbc
 
 def process_get(header_parameters, url_parameters):
 
+    if "security-key" in url_parameters:
 
+        if url_parameters["security-key"] == "YI64QZ8LMPJET0GV4CCV":
 
+            print("slave id is: ", url_parameters["slave-id"])
 
+            slave_id = url_parameters["slave-id"]
 
-    return "xxxxx"
+            if "register-session" in url_parameters:
+
+                print("need to register session", url_parameters["register-session"])
+
+                session = url_parameters["register-session"]
+
+                dbc.register_slave(session, slave_id)
+
+            if "register-user" in url_parameters:
+
+                print("need to register user", url_parameters["register-user"])
+                user = url_parameters["register-user"]
+                dbc.register_slave(user, slave_id)
+
+    message = "all good"
+    b = bytearray()
+    b.extend(map(ord, message))
+
+    return b
